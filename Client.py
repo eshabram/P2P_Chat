79435d45b5bfa91979ec1859a1b201d3eca00637
eshabram <elliotshabram@gmail.com>
2022-10-05 20:00:22 -0700

    edit

--- Client.py
+++ Client.py
@@ -10,23 +10,34 @@ max_rtt = 0
 avg_rtt = 0
 count_rtt = 0
 packet_loss = 0.0
-
+a = .125
+b = .25
+estimated_rtt = 0
+dev_rtt = 0
 # ping and wait for response or timeout
 for i in range(10):
-    message = 'Ping' + str(i+1)
+    message = 'Ping' + str(i + 1)
     clientSocket.sendto(message.encode(), (serverName, serverPort))
     time_sent = time.time()  # store the time message was sent
     try:
         clientSocket.settimeout(t_out)
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
         time_recv = time.time()  # store time for message received
-        rtt = (time_recv - time_sent)*1000
+        rtt = (time_recv - time_sent) * 1000
         rttString = str(rtt)
         print("\nMesg sent: " + message)
         print("Mesg rcvd: " + modifiedMessage.decode())
-        print("PONG {} RTT: {}ms".format(str(i+1), rttString))
+        print("PONG {} RTT: {}ms".format(str(i + 1), rttString))
         avg_rtt += rtt
         count_rtt += 1
+
+        # calculation for EstimatedRTT
+        estimated_rtt += (1 - a) * estimated_rtt + a * rtt
+
+        # calc for deviation Rtt
+        dev_rtt = (1 - b) * dev_rtt + b * abs(rtt - estimated_rtt)
+
+        # check for min and max value
         if rtt < min_rtt:
             min_rtt = rtt
         elif rtt > max_rtt:
@@ -35,7 +46,7 @@ for i in range(10):
     except TimeoutError as e:
         print("\nMesg sent: " + message)
         print("No Mesg rcvd")
-        print("PONG " + str(i+1) + " Request Timed out")
+        print("PONG " + str(i + 1) + " Request Timed out")
         packet_loss += 1.0
         continue
 
@@ -43,8 +54,10 @@ for i in range(10):
 avg_rtt /= count_rtt
 a = 0.125
 b = 0.25
-packet_loss = packet_loss/10*100
-# est_rtt = (1 - a)*
+packet_loss = packet_loss / 10 * 100
+
+timeout = estimated_rtt + 4 * dev_rtt
+# estimated_rtt = (1 - a)*
 # dev_rtt =
 
 print('\nMin RTT:         {} ms'.format(min_rtt))
